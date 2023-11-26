fb5ed6062f8c86fe4114c2660eeb6bb518e8db0b
David Reed <davidr@paragontruss.com>
2023-11-25 19:03:08 -0600

    camelCase

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import (
 )
 
 func rayColor(r Ray) Vec3 {
-	unit_direction := r.direction.UnitVector()
-	a := 0.5 * (unit_direction.Y() + 1.0)
+	unitDirection := r.direction.UnitVector()
+	a := 0.5 * (unitDirection.Y() + 1.0)
 
 	white := MakeVec3(1.0, 1.0, 1.0)
 	blue := MakeVec3(0.5, 0.7, 1.0)
@@ -19,47 +19,47 @@ func main() {
 
 	// Image
 
-	aspect_ratio := 16.0 / 9.0
+	aspectRatio := 16.0 / 9.0
 
-	image_width := 400
-	image_height := int(float64(image_width) / aspect_ratio)
+	imageWidth := 400
+	imageHeight := int(float64(imageWidth) / aspectRatio)
 
 	// Camera
 
-	focal_length := 1.0
-	viewport_height := 2.0
-	viewport_width := viewport_height * (float64(image_width) / float64(image_height))
-	camera_center := MakeVec3(0, 0, 0)
+	focalLength := 1.0
+	viewportHeight := 2.0
+	viewportWidth := viewportHeight * (float64(imageWidth) / float64(imageHeight))
+	cameraCenter := MakeVec3(0, 0, 0)
 
-	viewport_u := MakeVec3(viewport_width, 0, 0)
-	viewport_v := MakeVec3(0, viewport_height, 0)
+	viewportU := MakeVec3(viewportWidth, 0, 0)
+	viewportV := MakeVec3(0, viewportHeight, 0)
 
-	pixel_delta_u := viewport_u.DivideScalar(float64(image_width))
-	pixel_delta_v := viewport_v.DivideScalar(float64(image_height))
+	pixelDeltaU := viewportU.DivideScalar(float64(imageWidth))
+	pixelDeltaV := viewportV.DivideScalar(float64(imageHeight))
 
-	viewport_upper_left := camera_center.
-		Subtract(MakeVec3(0, 0, focal_length)).
-		Subtract(viewport_u.DivideScalar(2)).
-		Subtract(viewport_v.DivideScalar(2))
+	viewportUpperLeft := cameraCenter.
+		Subtract(MakeVec3(0, 0, focalLength)).
+		Subtract(viewportU.DivideScalar(2)).
+		Subtract(viewportV.DivideScalar(2))
 
-	pixel00_loc := viewport_upper_left.Add(pixel_delta_u.Add(pixel_delta_v).MultiplyScalar(0.5))
+	pixel00Loc := viewportUpperLeft.Add(pixelDeltaU.Add(pixelDeltaV).MultiplyScalar(0.5))
 
-	fmt.Printf("P3\n%d %d\n255\n", image_width, image_height)
+	fmt.Printf("P3\n%d %d\n255\n", imageWidth, imageHeight)
 
-	for y := image_height - 1; y >= 0; y-- {
+	for y := imageHeight - 1; y >= 0; y-- {
 		fmt.Fprintf(os.Stderr, "\rScanlines remaining: %d ", y)
 
-		for x := 0; x < image_width; x++ {
-			pixel_center := pixel00_loc.
-				Add(pixel_delta_u.MultiplyScalar(float64(x))).
-				Add(pixel_delta_v.MultiplyScalar(float64(y)))
+		for x := 0; x < imageWidth; x++ {
+			pixelCenter := pixel00Loc.
+				Add(pixelDeltaU.MultiplyScalar(float64(x))).
+				Add(pixelDeltaV.MultiplyScalar(float64(y)))
 
-			ray_direction := pixel_center.Subtract(camera_center)
+			rayDirection := pixelCenter.Subtract(cameraCenter)
 
-			ray := MakeRay(camera_center, ray_direction)
+			ray := MakeRay(cameraCenter, rayDirection)
 
-			pixel_color := rayColor(ray)
-			pixel_color.WriteAsColor()
+			pixelColor := rayColor(ray)
+			pixelColor.WriteAsColor()
 		}
 	}
 
