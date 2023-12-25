269bdc0ea3c5b88d9e934dba63db79f6eb59e807
David Reed <davidr@paragontruss.com>
2023-12-25 14:40:31 -0600

    implement book cover scene

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ var threads = max(1, runtime.NumCPU()-2)
 
 const samples = 500
 
-func renderScene(parameters cameraParameters) *render {
+func makeTripleSphereWorld() (hittable, cameraParameters) {
 
 	material_ground := lambertian{makeVec3(0.8, 0.8, 0.0)}
 
@@ -29,8 +29,73 @@ func renderScene(parameters cameraParameters) *render {
 	world.add(makeSphere(makeVec3(-1, 0, -1), -0.4, material_left))
 	world.add(makeSphere(makeVec3(1, 0, -1), 0.5, material_right))
 
-	camera := makeCamera(parameters)
+	parameters := cameraParameters{
+		imageWidth:  640, // 256,
+		imageHeight: 480, // 144,
 
+		vFov:     30,
+		lookFrom: makeVec3(-2, 2, 1),
+		lookAt:   makeVec3(0, 0, -1),
+		vUp:      makeVec3(0, 1, 0),
+
+		defocusAngle: 10,
+		focusDist:    3.4,
+	}
+
+	return world, parameters
+}
+
+func makeBook1CoverWorld() (hittable, cameraParameters) {
+
+	world := makeHittableList()
+
+	// ground
+	world.add(makeSphere(makeVec3(0, -1000, 0), 1000, lambertian{makeVec3(0.8, 0.8, 0.0)}))
+
+	for a := -11; a < 11; a++ {
+		for b := -11; b < 11; b++ {
+			chooseMat := rand()
+			center := makeVec3(float64(a)+0.9*rand(), 0.2, float64(b)+0.9*rand())
+
+			if center.subtract(makeVec3(4, 0.2, 0)).length() > 0.9 {
+				if chooseMat < 0.8 {
+					// diffuse
+					world.add(makeSphere(center, 0.2, lambertian{randVec().multiply(randVec())}))
+				} else if chooseMat < 0.95 {
+					// metal
+					world.add(makeSphere(center, 0.2, metal{randVecB(0.5, 1), randb(0, 0.5)}))
+				} else {
+					// glass
+					world.add(makeSphere(center, 0.2, dielectric{1.5}))
+				}
+			}
+		}
+	}
+
+	world.add(makeSphere(makeVec3(0, 1, 0), 1.0, dielectric{1.5}))
+	world.add(makeSphere(makeVec3(-4, 1, 0), 1.0, lambertian{makeVec3(0.4, 0.2, 0.1)}))
+	world.add(makeSphere(makeVec3(4, 1, 0), 1.0, metal{makeVec3(0.7, 0.6, 0.5), 0.0}))
+
+	parameters := cameraParameters{
+		imageWidth:  640, // 256,
+		imageHeight: 480, // 144,
+
+		vFov:     30,
+		lookFrom: makeVec3(13, 2, 3),
+		lookAt:   makeVec3(0, 0, 0),
+		vUp:      makeVec3(0, 1, 0),
+
+		defocusAngle: 0.6,
+		focusDist:    10.0,
+	}
+
+	return world, parameters
+}
+
+func renderScene() *render {
+
+	world, parameters := makeBook1CoverWorld()
+	camera := makeCamera(parameters)
 	render := camera.render(world)
 
 	if threaded {
@@ -57,21 +122,8 @@ func renderScene(parameters cameraParameters) *render {
 // const imageWidth = 256
 // const aspectRatio = 16.0 / 9.0
 
-var parameters = cameraParameters{
-	imageWidth:  640, // 256,
-	imageHeight: 480, // 144,
-
-	vFov:     30,
-	lookFrom: makeVec3(-2, 2, 1),
-	lookAt:   makeVec3(0, 0, -1),
-	vUp:      makeVec3(0, 1, 0),
-
-	defocusAngle: 10,
-	focusDist:    3.4,
-}
-
 func ppmMain() {
-	render := renderScene(parameters)
+	render := renderScene()
 
 	// TODO: progress reporting
 
@@ -85,7 +137,7 @@ func ppmMain() {
 }
 
 func ebitenMain() {
-	render := renderScene(parameters)
+	render := renderScene()
 
 	// TODO: progress reporting
 
