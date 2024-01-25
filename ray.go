badd2b5024ca6edca3e5ed94caed0cb0f70c0b95
David Reed <davidr@paragontruss.com>
2024-01-24 19:02:19 -0500

    add time to ray

--- ray.go
+++ ray.go
@@ -3,10 +3,15 @@ package main
 type ray struct {
 	origin    vec3
 	direction vec3
+	time      float64
 }
 
 func makeRay(origin, direction vec3) ray {
-	return ray{origin, direction}
+	return ray{origin, direction, 0.0}
+}
+
+func makeTimedRay(origin, direction vec3, time float64) ray {
+	return ray{origin, direction, time}
 }
 
 func (r ray) at(t float64) vec3 {
